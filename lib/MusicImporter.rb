1500feaf0b3deb3571c4d9afe7b9707adeb7703a
Warren Yao <warrenyca905@gmail.com>
2019-06-23 21:05:50 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -10,4 +10,8 @@ class MusicImporter
     Dir["#{path}/*"].each {|file| normal << file.reverse.chomp("/#{@path.reverse}").reverse}
     return normal
   end
+  
+  def import
+    files.each {|file| Song.create_from_filename(file)}
+  end
 end
