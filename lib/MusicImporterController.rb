a5e0859cb5299fc20068e13ede485d3c1ad3b735
Warren Yao <warrenyca905@gmail.com>
2019-06-23 23:44:55 +0000

    Automatically backed up by Learn

--- lib/MusicImporterController.rb
+++ lib/MusicImporterController.rb
@@ -25,6 +25,16 @@ class MusicLibraryController
       case command
       when "list songs"
         list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_artist
+      when "list genre"
+        list_genre
+      when "play song"
+        play_song
         
       command = gets
     end
