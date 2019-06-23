c248b6df9f359bb6d7d3f17f3eeda68eefc771d7
Warren Yao <warrenyca905@gmail.com>
2019-06-23 23:46:27 +0000

    Done.

--- lib/MusicImporterController.rb
+++ lib/MusicImporterController.rb
@@ -22,6 +22,20 @@ class MusicLibraryController
     
     command = gets
     while command != "exit" do
+      case command
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
       command = gets
     end
   end
